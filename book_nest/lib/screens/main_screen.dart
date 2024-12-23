30894df5688a676c71b681298423e00d1c8fb645
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-12-23 13:03:20 +0700

    update main_screen.dart

--- book_nest/lib/screens/main_screen.dart
+++ book_nest/lib/screens/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:book_nest/screens/favorite_screen.dart';
 import 'package:book_nest/screens/home_screen.dart';
+import 'package:book_nest/screens/favorite_screen.dart';
 import 'package:book_nest/screens/history_screen.dart';
 
 class MainScreen extends StatefulWidget {
@@ -13,10 +13,10 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
 
-  static const List<Widget> _screenOptions = <Widget>[
-    HomeScreen(),
-    FavoriteScreen(),
-    HistoryScreen(),
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const FavoriteScreen(),
+    const HistoryScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -28,23 +28,24 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screenOptions.elementAt(_selectedIndex),
+      body: _screens[_selectedIndex], // Display the selected screen
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          //item pertama
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          //item kedua
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), label: 'Favorite'),
-          //item ketiga
-          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
-        ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedItemColor: Colors.greenAccent,
-        unselectedItemColor: Colors.blueGrey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+        ],
       ),
     );
   }
