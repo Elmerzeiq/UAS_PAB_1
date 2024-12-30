11b136e4e03937198890dd728798c35765af6349
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-12-30 17:38:47 +0700

    add user

--- book_nest/lib/data/user_data.dart
+++ book_nest/lib/data/user_data.dart
@@ -4,6 +4,14 @@ var userList = {
   User(
       username: 'Anas Marzaq',
       email: 'anasmarzaq@gmail.com',
-      password: '123456')
+      password: '123456'),
+  User(
+      username: 'Michael',
+      email: 'michael@gmail.com',
+      password: '123457'),
+  User(
+      username: 'Jenifer Gunawan',
+      email: 'jenifergunawan@gmail.com',
+      password: '123458')
       // url_launcher
 };
